f63496e45d655fb04bd2654a8dd2bf234cc342df
Eric Mueller <emueller@emcien.com>
2013-10-13 18:07:26 +0000

    fix/improve logic for listing available upload dirs

--- app/models/gallery_list.rb
+++ app/models/gallery_list.rb
@@ -3,23 +3,31 @@ class GalleryList
     dir = Dir.new $UPLOAD_ROOT
 
     avail = []
-    dir.each do |entry|
-      path = File.join($UPLOAD_ROOT, entry)
-      next unless Dir.exist?(path)
+    each_subpath($UPLOAD_ROOT) do |path|
       next unless finished_uploading?(path)
       avail << File.basename(path)
     end
-
     avail.sort
   end
 
+  def self.each_subpath(path, &block)
+    dir = Dir.new path
+    return nil unless Dir.exist?(path)
+
+    dir.each do |entry|
+      next if entry =~ /^\./
+      next unless File.exist?(path)
+      subpath = File.join(path, entry)
+      yield subpath
+    end
+  end
+
   def self.finished_uploading?(path)
-    last_mtime = File::Stat.new(path).mtime
-    Dir.new(path).each do |subentry|
-      subpath = File.join(path, subentry)
+    last_mtime = 1.day.ago
+    each_subpath(path) do |subpath|
       this_mtime = File::Stat.new(subpath).mtime
       last_mtime = this_mtime if this_mtime > last_mtime
     end
-    last_mtime < 30.seconds.ago
+    last_mtime < 10.seconds.ago
   end
 end
